f327bfe328a9591c273ce1c0b866ac9f899c0f61
YUVALSARUSI1 <yuval4.2000@gmail.com>
2025-11-22 11:35:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import string_utils as yv, equation_utils as vl
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
-
+    
     # 1.parse reaction
     reactants, products = yv.parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
     reactant_atoms = yv.count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
